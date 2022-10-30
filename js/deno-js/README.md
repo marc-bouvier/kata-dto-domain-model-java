4758a4c5787be33447d4233e664db8a5164c9880
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-10-30 15:40:56 +0100

    cleanup non necessary versions

--- js/deno-js/README.md
+++
@@ -1,9 +0,0 @@
-# Deno
-
-Install deno : https://deno.land/#installation
-
-## Run tests
-
-```shell
-deno test
-```
--- js/deno-ts/tests/helloWorld.test.ts
+++
@@ -1,7 +0,0 @@
-// helloWorld.test.ts
-import { assertEquals } from "https://deno.land/std@0.152.0/testing/asserts.ts";
-import { hello } from "../lib/helloWorld.ts";
-
-Deno.test("helloWord test", () => {
-  assertEquals(hello("World"), "Hello World!");
-});
